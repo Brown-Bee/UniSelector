2c2e20c630d8834ba24b2e35948efffd19047fe6
hodaifah AL salahi <yemeniq8@gmail.com>
2024-11-30 00:14:51 +0300

    Small changes

---
+++ UniSelector.Web/ViewComponents/EmailViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using UniSelector.DataAccess.Repository.IRepository;
+
+namespace UniSelector.Web.ViewComponents
+{
+    public class EmailViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EmailViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(string Id)
+        {
+            var user = await _unitOfWork.ApplicationUser.GetUserById(Id);
+            return View("Default", user.Email);
+        }
+    }
+}
