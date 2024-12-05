5027a86e5a2be3dbfe1d228ce413017382568cf1
hodaifah AL salahi <yemeniq8@gmail.com>
2024-12-05 23:47:56 +0300

    Add the remaining sections in Details.cshtml page

--- UniSelector.Models/Major.cs
+++ UniSelector.Models/Major.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
@@ -10,25 +11,28 @@ public class Major
 
     // Basic Info
     public int Credits { get; set; }
+    [DisplayName("Average Price")]
     public double AveragePrice { get; set; }
-    
+    [DisplayName("Minimum Grade")]
     [Range(0, 100)]
     public double MinimumGrade { get; set; }
 
 
     // Testing Requirements
+    [DisplayName("Minimum IELTS")]
     [Range(0, 9)]
     public double? MinimumIELTS { get; set; }
-
+    [DisplayName("Minimum TOEFL")]
     [Range(0, 120)]
     public double? MinimumTOEFL { get; set; }
-
+    [DisplayName("Requires Aptitude Test")]
     public bool RequiresAptitudeTest { get; set; }
 
     // Job Market Info
+    [DisplayName("Employment Rate")]
     [Range(0, 100)]
     public double EmploymentRate { get; set; }
-
+    [DisplayName("Average Starting Salary")]
     public double AverageStartingSalary { get; set; }
 
 
